69f4e8c0d4d14fb0a31f41b11ac9207d968d9927
Akanksha Shopra <akankshashopra@gmail.com>
2023-06-28 15:26:16 +0530

    show status message and show the winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './styles.scss';
 import { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
   const [squares, setsquares] = useState(Array(9).fill(null));
   const [isXNext, setIsNext] = useState(false);
 
+  const winner = calculateWinner(squares);
   const nextPlayer = isXNext ? 'X' : '0';
+  const statusmessage = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${nextPlayer}`;
 
   const handleSquareClick = clickedposition => {
-    if (squares[clickedposition]) {
+    if (squares[clickedposition] || winner) {
       return;
     }
     setsquares(currentSquares => {
@@ -27,7 +32,7 @@ function App() {
 
   return (
     <div className="app">
-      <h2>Next player somebody {nextPlayer}</h2>
+      <h2> {statusmessage}</h2>
       <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
